3a27e72f35d8ec4ef828574132afd9ae85a6c7fa
Luis Cedillo Maldonado <luis.cedillo.maldonado@gmail.com>
2024-01-04 21:44:40 -0600

    fix: removed users temporarily from urls

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -1,8 +1,50 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+import uuid
 
 
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
+class ActiveManager(models.Manager):
+    def get_queryset(self):
+        # Filter the queryset to only include active (non-deleted) objects
+        return super().get_queryset().filter(status=BaseModel.Status.ACTIVE)
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("Created At"))
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name=_("Updated At"))
+    status = models.CharField(
+        max_length=10, default='active', verbose_name=_("Status"))
+
+    class Status(models.TextChoices):
+        ACTIVE = 'active', _("Active")
+        DELETED = 'deleted', _("Deleted")
+        ARCHIVED = 'archived', _("Archived")
+
+    class Meta:
+        abstract = True
+
+    def delete(self):
+        self.status = self.Status.DELETED
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def hard_delete(self):
+        super(BaseModel, self).delete()
+
+    def restore(self):
+        self.status = self.Status.ACTIVE
+        self.save()
+
+    def get_active(self):
+        return self.__class__.objects.filter(status=self.Status.ACTIVE)
+
+    objects = models.Manager()  # Default manager
+    active_objects = ActiveManager()  # Custom manager for active objects only
+
+    def __str__(self):
+        return f"{self.__class__.__name__} {self.id}"
