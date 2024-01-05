7dd0bb875bbb3b0d9f6b79d174a4c48b61ea0486
Luis Cedillo Maldonado <luis.cedillo.maldonado@gmail.com>
2024-01-04 22:41:49 -0600

    feat: added user model

--- djangoapp/users/models.py
+++ djangoapp/users/models.py
@@ -1,5 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 from django.db import models
+# Assuming BaseModel is in a file called base/models.py
+from .models import BaseModel, ActiveManager
 
-'''
-Model for the uploaded class schedules
-'''
+
+class User(AbstractUser, BaseModel):
+    # Assuming email is the unique identifier
+    email = models.EmailField(_('email address'), unique=True)
+    name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+
+    # Other custom fields can be added here
+
+    active_objects = ActiveManager()
+
+    class Meta(AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
